f435a178d9537912e273cba64f848d8374bb4989
daynwort <gurzhiyroman@gmail.com>
2020-12-01 15:25:50 +0200

    Add infrastructure

---
+++ src/test/java/Test1.java
@@ -0,0 +1,10 @@
+import infrastructure.base.TestBase;
+import org.junit.Test;
+
+public class Test1 extends TestBase {
+
+    @Test
+    public void firstTest(){
+        driver.get("https://www.vodafone.ua");
+    }
+}
